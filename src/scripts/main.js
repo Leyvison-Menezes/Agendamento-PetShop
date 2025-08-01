5e48065b11e31aab591a92fdb4d9196d3bbdeae0
Menezes <leyvisonmenezes@gmail.com>
2025-08-01 09:33:40 -0300

    add funcionalidades no form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,15 @@
 "use strict"
 
+// Libs
 import "../lib/dayjs.js"
 
+// CSS
 import "../styles/global.css"
 import "../styles/layout.css"
 import "../styles/overlay.css"
 import "../styles/periods.css"
 import "../styles/utilities.css"
+
+// JS
+import "../scripts/form/submit.js"
+import "../scripts/form/open-close-form.js"
